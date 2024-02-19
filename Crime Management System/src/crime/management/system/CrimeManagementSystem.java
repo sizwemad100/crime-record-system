3f36a516a57786bfb89228b192218cf3674c19f7
sizwemad100 <85628148+sizwemad100@users.noreply.github.com>
2024-02-19 15:01:42 +0200

    Add files via upload

---
+++ Crime Management System/src/crime/management/system/CrimeManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crime.management.system;
+
+/**
+ *
+ * @author SizweM
+ */
+public class CrimeManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Mainpage form = new Mainpage();
+        form.setVisible(true);
+    }
+    
+}
---
+++ Crime Management System/src/crime/management/system/conn.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crime.management.system;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author SizweM
+ */
+class conn {
+
+    static PreparedStatement prepareStatement(String q) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
